6e3966d028f2de4723cfa193044f20a1633cd289
Volatile_xian <x125924@163.com>
2013-08-19 17:59:46 +0800

    update the ui

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -7,6 +7,7 @@ MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent), ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    ui->buttonENG->setStyleSheet("background-color: rgb(0, 0, 0);");
 }
 
 MainWindow::~MainWindow()
